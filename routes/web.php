5a63e1a1b6749cd5bf804f57a48f2480714d7f07
LeoZion <leozionsuporte@gmail.com>
2022-07-10 05:13:29 -0300

    add login component

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,14 @@ use App\Http\Livewire\Admin\{
     Task,
     User
 };
+use App\Http\Livewire\Admin\Auth\Login;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Dashboard::class)->name('admin.dashboard');
-Route::get('/categories', Category::class)->name('admin.categories');
-Route::get('/tasks', Task::class)->name('admin.tasks');
-Route::get('/users', User::class)->name('admin.users');
+Route::get('/admin', Login::class)->name('admin.login');
+
+Route::prefix('admin')->group(function() {
+    Route::get('/dashboard', Dashboard::class)->name('admin.dashboard');
+    Route::get('/categories', Category::class)->name('admin.categories');
+    Route::get('/tasks', Task::class)->name('admin.tasks');
+    Route::get('/users', User::class)->name('admin.users');
+});
